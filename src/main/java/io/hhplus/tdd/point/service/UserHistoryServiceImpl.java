5a13aadfef9280e01ed8709db0c181a382cbe033
kssim <kssim@inzisoft.com>
2025-10-22 09:26:28 +0900

    refactor : UserHistory서비스 -> UserPoint서비스로 변경(database , 레포지토리와 이름 통일)

--- src/main/java/io/hhplus/tdd/point/service/UserHistoryServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package io.hhplus.tdd.point.service;
-
-public class UserHistoryServiceImpl implements PointHistoryService {
-}
--- src/main/java/io/hhplus/tdd/point/service/UserPointService.java
+++ src/main/java/io/hhplus/tdd/point/service/UserPointService.java
@@ -1,4 +1,4 @@
 package io.hhplus.tdd.point.service;
 
-public interface UserHistoryService {
+public interface UserPointService {
 }
