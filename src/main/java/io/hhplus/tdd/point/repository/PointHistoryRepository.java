61286a5426a9905d694d4971427f096fda76a590
kssim <kssim@inzisoft.com>
2025-10-22 17:41:41 +0900

    feat : 포인트 히스토리 레포지토리 생성

--- src/main/java/io/hhplus/tdd/point/repository/PointHistoryRepository.java
+++ src/main/java/io/hhplus/tdd/point/repository/PointHistoryRepository.java
@@ -1,4 +1,10 @@
 package io.hhplus.tdd.point.repository;
 
+import io.hhplus.tdd.point.domain.PointHistory;
+
+import java.util.List;
+
 public interface PointHistoryRepository {
+    List<PointHistory> getHistoryById(Long userId);
+    PointHistory addHistory(PointHistory pointHistory);
 }
